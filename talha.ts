56b33a85a95a8d9b8e09f40861b7911608b014dd
Sunny-Janjua <144367599+Sunny-Janjua@users.noreply.github.com>
2023-09-12 21:13:38 +0500

    Add files via upload

---
+++ talha.ts
@@ -0,0 +1,32 @@
+
+var cars=[{
+    company_name:"ajaz",
+    roll_no:1,
+ },
+ {
+    company_name:"sunny",
+    roll_no:2,
+ },
+ {
+    company_name:"janjua",
+    roll_no:3,
+ },
+ {
+    company_name:"mulazam",
+    roll_no:4,
+ },
+ {
+    company_name:"hussain",
+    roll_no:5,
+ },
+ {
+    company_name:"asad",
+    roll_no:6,
+ },
+ {
+    company_name:"awais",
+    roll_no:7,
+ }
+ ];
+ for(var i=0; i<cars.length; i++)
+ console.log("This is Car name and number list:",cars[i].company_name);
