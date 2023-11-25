651ef7e09a362fb5f85accd6358cebf6b4f13fb7
Hussnain Mulazam <144367599+Sunny-Janjua@users.noreply.github.com>
2023-11-25 08:12:11 -0800

    Add files via upload

--- page.tsx
+++ page.tsx
@@ -1,7 +1,7 @@
-function Contact() {
+function Contact(){
     return (
         <main>
-            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" />
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css"/>
             <nav className="bg-gray-800 p-4">
                 <div className="container mx-auto flex justify-between items-center">
                     <div className="text-2xl font-bold text-white">Code With Sunny</div>
@@ -13,51 +13,49 @@ function Contact() {
                     </ul>
                 </div>
             </nav>
-            <section className="bg-blue-500 text-white h-screen flex items-center">
-                <div className="container mx-auto text-center">
-                    <h1 className="text-5xl font-bold mb-4">About Our Company</h1>
-                    <p className="text-lg">Learn more about who we are and what we do.</p>
+    <div className="container mx-auto mt-8">
+        <section className="mb-8">
+            <h2 className="text-2xl font-semibold mb-4">Send us a message</h2>
+            <form action="#" method="post" className="max-w-lg">
+                <div className="mb-4">
+                    <label className="block text-gray-700"/>Name:
+                    <input type="text" id="name" name="name" className="w-full border p-2"/>
                 </div>
-            </section>
-            <section className="py-12">
-                <div className="container mx-auto">
-                    <h2 className="text-3xl font-bold mb-6">Our Story</h2>
-                    <p className="text-gray-700">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sit amet risus eget nisl bibendum tristique.
-                        Proin vitae libero vel orci ullamcorper tristique. Donec lacinia, mi a scelerisque efficitur, elit ex hendrerit purus,
-                        in laoreet tellus nisi id dui.
-                    </p>
-                    <p className="text-gray-700">
-                        Nunc vel massa nec urna tincidunt congue. Sed ac tortor aliquam, facilisis turpis et, laoreet massa. Aenean euismod,
-                        purus non tempus tincidunt, nunc sapien convallis quam, a vestibulum nunc justo vel nulla.
-                    </p>
+
+                <div className="mb-4">
+                    <label className="block text-gray-700"/>Email:
+                    <input type="email" id="email" name="email" className="w-full border p-2"/>
                 </div>
-            </section>
-            <section className="bg-gray-200 py-12">
-                <div className="container mx-auto">
-                    <h2 className="text-3xl font-bold mb-6">Our Team</h2>
-                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-                        <div className="bg-white p-6 rounded-md shadow-md">
-                            <h3 className="text-xl font-bold mb-2">John Doe</h3>
-                            <p className="text-gray-700">Founder & CEO</p>
-                        </div>
-                        <div className="bg-white p-6 rounded-md shadow-md">
-                            <h3 className="text-xl font-bold mb-2">Jane Smith</h3>
-                            <p className="text-gray-700">COO</p>
-                        </div>
-                        <div className="bg-white p-6 rounded-md shadow-md">
-                            <h3 className="text-xl font-bold mb-2">Bob Johnson</h3>
-                            <p className="text-gray-700">CTO</p>
-                        </div>
-                    </div>
+
+                <div className="mb-4">
+                    <label  className="block text-gray-700"/>Message:
+                    <textarea id="message" name="message" className="w-full border p-2"/>
                 </div>
-            </section>
-            <footer className="bg-gray-800 text-white p-4">
+
+                <button type="submit" className="bg-blue-500 text-white px-4 py-2">Send Message</button>
+            </form>
+        </section>
+        <section>
+            <h2 className="text-2xl font-semibold mb-4">Our Location</h2>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+                <div className="mb-4 md:mb-0">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d158858.79472579783!2d-122.41941596475424!3d37.77492948933341!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808f7e16ddd61833%3A0x76b792801138a8b8!2sSan%20Francisco%2C%20CA!5e0!3m2!1sen!2sus!4v1631397226121!5m2!1sen!2sus" width="100%" height="300"/>
+                </div>
+                <div>
+                    <p className="text-gray-700"><strong>Address:</strong> 123 Main Street, Cityville, State, Country</p>
+                    <p className="text-gray-700"><strong>Email:</strong> info@example.com</p>
+                    <p className="text-gray-700"><strong>Phone:</strong> +1 (123) 456-7890</p>
+                </div>
+            </div>
+        </section>
+
+    </div>
+    <footer className="bg-gray-800 text-white p-4">
                 <div className="container mx-auto text-center">
                     <p>&copy; 2023 Your Company. All rights reserved.</p>
                 </div>
             </footer>
         </main>
-    )
+        )
 }
 export default Contact
